a709b67274fb498f812f0634f773e6cb4c47b6e5
chaoqiang <chaoqiang@staff.weibo.com>
2016-01-21 09:43:09 +0800

    Executor and DataMonitor

---
+++ src/main/java/skyler/tao/zookeeper/SimpleClientMain.java
@@ -0,0 +1,13 @@
+package skyler.tao.zookeeper;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleClientMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
